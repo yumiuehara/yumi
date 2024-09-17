3940a931fb7c8c8b51b42beba8e1ce26467be243
yumi <yumiueharac@gmail.com>
2024-09-17 03:41:02 -0300

    fix tailwind bg image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="w-full h-[100vh] flex justify-center items-end bg-[url('./images/bg.webp')] bg-center bg-no-repeat bg-cover">
+    <div className="w-full h-[100vh] flex justify-center items-end bg-landing-page bg-center bg-no-repeat bg-cover">
       <span className="break-all text-center font-bold md:text-[8rem] text-[2rem] text-white [text-shadow:_0_2px_4px_rgb(0_0_0_/_0.8)] py-5">
         yumi.dev.br
       </span>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
       },
+      backgroundImage: {
+        'landing-page': "url('/images/bg.webp')"
+      }
     },
   },
   plugins: [],
