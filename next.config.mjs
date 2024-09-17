1c66c3b9ef3986f139c07b49fc48bac7b3962291
yumi <yumiueharac@gmail.com>
2024-09-17 03:05:53 -0300

    fix next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const isProduction = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
+    basePath: isProduction ? '/yumi' : '',
     output: "export",
     images: {
         unoptimized: true
